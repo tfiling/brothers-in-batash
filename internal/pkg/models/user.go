822caad7737c5a0d5d0bc935b2f5060f24bdb8af
galt <galtfilin@gmail.com>
2024-05-08 18:52:32 +0300

    User model and data access implementation

---
+++ internal/pkg/models/user.go
@@ -0,0 +1,6 @@
+package models
+
+type User struct {
+	Username       string `validate:"alphanum,ascii,min=4,max=100"`
+	HashedPassword []byte `validate:"ascii,min=4,max=100"`
+}
---
+++ internal/pkg/store/user.go
@@ -0,0 +1,40 @@
+package store
+
+import (
+	"brothers_in_batash/internal/pkg/models"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/pkg/errors"
+)
+
+type IUserStore interface {
+	CreateNewUser(user models.User) error
+	FindUserByUsername(username string) ([]models.User, error)
+}
+
+type InMemUserStore struct {
+	users map[string]models.User
+}
+
+func NewUserStore() (*InMemUserStore, error) {
+	return &InMemUserStore{users: make(map[string]models.User)}, nil
+}
+
+func (us *InMemUserStore) CreateNewUser(user models.User) error {
+	if err := validator.New().Struct(user); err != nil {
+		return errors.Wrap(err, "user validation failed")
+	}
+	if _, exists := us.users[user.Username]; exists {
+		return errors.New("user already exists")
+	}
+	us.users[user.Username] = user
+	return nil
+}
+
+func (us *InMemUserStore) FindUserByUsername(username string) ([]models.User, error) {
+	if res, exists := us.users[username]; !exists {
+		return []models.User{}, nil
+	} else {
+		return []models.User{res}, nil
+	}
+}
