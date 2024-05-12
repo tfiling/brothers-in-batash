09af2beb2dae07f7b979adc377250e3b5aa55c8a
galt <galtfilin@gmail.com>
2024-05-12 14:52:13 +0300

    Fake Protected API endpoint

--- internal/app/webserver/controllers/hello.go
+++ internal/app/webserver/controllers/hello.go
@@ -1,6 +1,13 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"brothers_in_batash/internal/pkg/logging"
+	jwtmw "brothers_in_batash/internal/pkg/middleware/jwt"
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	jtoken "github.com/golang-jwt/jwt/v5"
+)
 
 type HelloController struct {
 	router fiber.Router
@@ -15,4 +22,13 @@ func (c *HelloController) SetupRoutes() error {
 		return c.SendString("Hello, World!")
 	})
 	return nil
+}
+
+func (c *HelloController) ProtectedHello(ctx *fiber.Ctx) error {
+	logging.Info("protected hello", nil)
+	token := ctx.Locals(jwtmw.ContextKey).(*jtoken.Token)
+	logging.Info(fmt.Sprintf("token: %v", token), nil)
+	claims := token.Claims.(jtoken.MapClaims)
+	logging.Info(fmt.Sprintf("claims: %v", claims), nil)
+	return ctx.SendString("Welcome 👋")
 }
