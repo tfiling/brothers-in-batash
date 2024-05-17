8db16d376eda8a93f60ccc861d339e08740ef7df
galt <galtfilin@gmail.com>
2024-05-18 02:46:00 +0300

    Soldier controller exposing CRUD API

---
+++ internal/app/webserver/controllers/soldier_controller.go
@@ -0,0 +1,91 @@
+package controllers
+
+import (
+	"brothers_in_batash/internal/pkg/logging"
+	"brothers_in_batash/internal/pkg/models"
+	"brothers_in_batash/internal/pkg/store"
+	"errors"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type SoldierController struct {
+	soldierStore store.ISoldierStore
+}
+
+func NewSoldierController(soldierStore store.ISoldierStore) (*SoldierController, error) {
+	if soldierStore == nil {
+		return nil, errors.New("soldierStore is nil")
+	}
+	return &SoldierController{soldierStore: soldierStore}, nil
+}
+
+func (c *SoldierController) RegisterRoutes(router fiber.Router) error {
+	router.Post(CreateSoldierRoute, c.createSoldier)
+	router.Get(GetSoldierRoute, c.getSoldier)
+	router.Get(GetAllSoldiersRoute, c.getAllSoldiers)
+	router.Put(UpdateSoldierRoute, c.updateSoldier)
+	router.Delete(DeleteSoldierRoute, c.deleteSoldier)
+	return nil
+}
+
+func (c *SoldierController) createSoldier(ctx *fiber.Ctx) error {
+	soldier := models.Soldier{}
+	if err := ctx.BodyParser(&soldier); err != nil {
+		logging.Debug("Could not parse soldier creation request body", []logging.LogProp{{"error", err.Error()}})
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	if err := c.soldierStore.CreateNewSoldier(soldier); err != nil {
+		logging.Warning(err, "error on creating new soldier", nil)
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+	return ctx.SendStatus(fiber.StatusCreated)
+}
+
+func (c *SoldierController) getSoldier(ctx *fiber.Ctx) error {
+	soldierID := ctx.Params("id")
+	soldiers, err := c.soldierStore.FindSoldierByID(soldierID)
+	if err != nil {
+		logging.Warning(err, "could not query for soldier", []logging.LogProp{{"soldierID", soldierID}})
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	} else if len(soldiers) == 0 {
+		logging.Trace("Soldier not found", []logging.LogProp{{"soldierID", soldierID}})
+		return ctx.SendStatus(fiber.StatusNotFound)
+	}
+	return ctx.JSON(soldiers[0])
+}
+
+func (c *SoldierController) getAllSoldiers(ctx *fiber.Ctx) error {
+	soldiers, err := c.soldierStore.FindAllSoldiers()
+	if err != nil {
+		logging.Warning(err, "error on fetching all soldiers", nil)
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+	return ctx.JSON(soldiers)
+}
+
+func (c *SoldierController) updateSoldier(ctx *fiber.Ctx) error {
+	soldierID := ctx.Params("id")
+	soldier := models.Soldier{}
+	if err := ctx.BodyParser(&soldier); err != nil {
+		logging.Info("Could not parse soldier update request body", []logging.LogProp{{"error", err.Error()}})
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	soldier.ID = soldierID
+	if err := c.soldierStore.UpdateSoldier(soldier); err != nil {
+		logging.Warning(err, "error on updating soldier", []logging.LogProp{{"soldierID", soldierID}})
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+	return ctx.SendStatus(fiber.StatusOK)
+}
+
+func (c *SoldierController) deleteSoldier(ctx *fiber.Ctx) error {
+	soldierID := ctx.Params("id")
+	if err := c.soldierStore.DeleteSoldier(soldierID); err != nil {
+		logging.Warning(err, "error on deleting soldier", []logging.LogProp{{"soldierID", soldierID}})
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+	return ctx.SendStatus(fiber.StatusOK)
+}
---
+++ internal/app/webserver/controllers/soldier_controller_test.go
@@ -0,0 +1,219 @@
+package controllers_test
+
+import (
+	"brothers_in_batash/internal/app/webserver/controllers"
+	"brothers_in_batash/internal/pkg/mocks"
+	"brothers_in_batash/internal/pkg/models"
+	"brothers_in_batash/internal/pkg/test_utils"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSoldierController_NewSoldierController__error_on_nil_store(t *testing.T) {
+	// Act
+	controller, err := controllers.NewSoldierController(nil)
+
+	// Assert
+	assert.Error(t, err)
+	assert.Nil(t, controller)
+}
+
+func TestSoldierController_NewSoldierController__success(t *testing.T) {
+	// Arrange
+	soldierStore := &mocks.MockISoldierStore{}
+
+	// Act
+	controller, err := controllers.NewSoldierController(soldierStore)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.NotNil(t, controller)
+}
+
+func TestSoldierController_CreateSoldier__invalid_request_body(t *testing.T) {
+	// Arrange
+	app := fiber.New()
+	soldierStore := &mocks.MockISoldierStore{}
+	controller, err := controllers.NewSoldierController(soldierStore)
+	require.NoError(t, err)
+	err = controller.RegisterRoutes(app)
+	require.NoError(t, err)
+	req := httptest.NewRequest(fiber.MethodPost, controllers.CreateSoldierRoute, test_utils.WrapStructWithReader(t, "invalid"))
+	req.Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+	// Act
+	resp, err := app.Test(req)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+	soldierStore.AssertExpectations(t)
+}
+
+func TestSoldierController_CreateSoldier__success(t *testing.T) {
+	// Arrange
+	app := fiber.New()
+	soldierStore := &mocks.MockISoldierStore{}
+	controller, err := controllers.NewSoldierController(soldierStore)
+	require.NoError(t, err)
+	err = controller.RegisterRoutes(app)
+	require.NoError(t, err)
+	soldier := models.Soldier{ID: "1", FirstName: "John", LastName: "Doe"}
+	soldierStore.On("CreateNewSoldier", soldier).Return(nil)
+	req := httptest.NewRequest(fiber.MethodPost, controllers.CreateSoldierRoute, test_utils.WrapStructWithReader(t, soldier))
+	req.Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+	// Act
+	resp, err := app.Test(req)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusCreated, resp.StatusCode)
+	soldierStore.AssertExpectations(t)
+}
+
+func TestSoldierController_GetSoldier__not_found(t *testing.T) {
+	// Arrange
+	app := fiber.New()
+	soldierStore := &mocks.MockISoldierStore{}
+	controller, err := controllers.NewSoldierController(soldierStore)
+	require.NoError(t, err)
+	err = controller.RegisterRoutes(app)
+	require.NoError(t, err)
+	soldierID := "1"
+	soldierStore.On("FindSoldierByID", soldierID).Return([]models.Soldier{}, nil)
+	req := httptest.NewRequest(fiber.MethodGet, "/soldiers/"+soldierID, nil)
+
+	// Act
+	resp, err := app.Test(req)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusNotFound, resp.StatusCode)
+	soldierStore.AssertExpectations(t)
+}
+
+func TestSoldierController_GetSoldier__success(t *testing.T) {
+	// Arrange
+	app := fiber.New()
+	soldierStore := &mocks.MockISoldierStore{}
+	controller, err := controllers.NewSoldierController(soldierStore)
+	require.NoError(t, err)
+	err = controller.RegisterRoutes(app)
+	require.NoError(t, err)
+	soldierID := "1"
+	soldier := models.Soldier{ID: soldierID, FirstName: "John", LastName: "Doe"}
+	soldierStore.On("FindSoldierByID", soldierID).Return([]models.Soldier{soldier}, nil)
+	req := httptest.NewRequest(fiber.MethodGet, "/soldiers/"+soldierID, nil)
+
+	// Act
+	resp, err := app.Test(req)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+	var respSoldier models.Soldier
+	err = json.NewDecoder(resp.Body).Decode(&respSoldier)
+	assert.NoError(t, err)
+	assert.Equal(t, soldier, respSoldier)
+	soldierStore.AssertExpectations(t)
+}
+
+func TestSoldierController_GetAllSoldiers__success(t *testing.T) {
+	// Arrange
+	app := fiber.New()
+	soldierStore := &mocks.MockISoldierStore{}
+	controller, err := controllers.NewSoldierController(soldierStore)
+	require.NoError(t, err)
+	err = controller.RegisterRoutes(app)
+	require.NoError(t, err)
+	soldiers := []models.Soldier{
+		{ID: "1", FirstName: "John", LastName: "Doe"},
+		{ID: "2", FirstName: "Jane", LastName: "Smith"},
+	}
+	soldierStore.On("FindAllSoldiers").Return(soldiers, nil)
+	req := httptest.NewRequest(fiber.MethodGet, controllers.GetAllSoldiersRoute, nil)
+
+	// Act
+	resp, err := app.Test(req)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+	var respSoldiers []models.Soldier
+	err = json.NewDecoder(resp.Body).Decode(&respSoldiers)
+	assert.NoError(t, err)
+	assert.Equal(t, soldiers, respSoldiers)
+	soldierStore.AssertExpectations(t)
+}
+
+func TestSoldierController_UpdateSoldier__invalid_request_body(t *testing.T) {
+	// Arrange
+	app := fiber.New()
+	soldierStore := &mocks.MockISoldierStore{}
+	controller, err := controllers.NewSoldierController(soldierStore)
+	require.NoError(t, err)
+	err = controller.RegisterRoutes(app)
+	require.NoError(t, err)
+	soldierID := "1"
+	req := httptest.NewRequest(fiber.MethodPut, "/soldiers/"+soldierID, test_utils.WrapStructWithReader(t, "invalid"))
+	req.Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+	// Act
+	resp, err := app.Test(req)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+	soldierStore.AssertExpectations(t)
+}
+
+func TestSoldierController_UpdateSoldier__success(t *testing.T) {
+	// Arrange
+	app := fiber.New()
+	soldierStore := &mocks.MockISoldierStore{}
+	controller, err := controllers.NewSoldierController(soldierStore)
+	require.NoError(t, err)
+	err = controller.RegisterRoutes(app)
+	require.NoError(t, err)
+	soldierID := "1"
+	soldier := models.Soldier{ID: soldierID, FirstName: "John", LastName: "Doe"}
+	soldierStore.On("UpdateSoldier", mock.AnythingOfType("models.Soldier")).Return(nil)
+	req := httptest.NewRequest(fiber.MethodPut, "/soldiers/"+soldierID, test_utils.WrapStructWithReader(t, soldier))
+	req.Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+	// Act
+	resp, err := app.Test(req)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+	soldierStore.AssertExpectations(t)
+}
+
+func TestSoldierController_DeleteSoldier__success(t *testing.T) {
+	// Arrange
+	app := fiber.New()
+	soldierStore := &mocks.MockISoldierStore{}
+	controller, err := controllers.NewSoldierController(soldierStore)
+	require.NoError(t, err)
+	err = controller.RegisterRoutes(app)
+	require.NoError(t, err)
+	soldierID := "1"
+	soldierStore.On("DeleteSoldier", soldierID).Return(nil)
+	req := httptest.NewRequest(fiber.MethodDelete, "/soldiers/"+soldierID, nil)
+
+	// Act
+	resp, err := app.Test(req)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+	soldierStore.AssertExpectations(t)
+}
