6ffd99100b576ac1ac1e699433849f7c03a62990
galt <galtfilin@gmail.com>
2024-05-18 02:46:22 +0300

    Make soldier ID optional for user

--- internal/pkg/models/user.go
+++ internal/pkg/models/user.go
@@ -3,5 +3,5 @@ package models
 type User struct {
 	Username       string `validate:"alphanum,ascii,min=4,max=100"`
 	HashedPassword []byte `validate:"ascii,min=4,max=100"`
-	SoldierID      string `validate:"required"`
+	SoldierID      string `validate:"omitempty"`
 }
