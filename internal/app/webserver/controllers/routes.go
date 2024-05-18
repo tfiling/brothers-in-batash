3d7533d5fdf3ef7d5c6ceb20b9b51e5322a0b11b
galt <galtfilin@gmail.com>
2024-05-18 04:07:37 +0300

    Register routes for controllers

--- internal/app/webserver/controllers/routes.go
+++ internal/app/webserver/controllers/routes.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"brothers_in_batash/internal/pkg/config"
+	"brothers_in_batash/internal/pkg/middleware/jwt"
 	"brothers_in_batash/internal/pkg/store"
 
 	"github.com/gofiber/fiber/v2"
@@ -37,6 +39,13 @@ type Controller interface {
 	RegisterRoutes(router fiber.Router) error
 }
 
+type storeInstancesContainer struct {
+	dayStore     store.IDayStore
+	shiftStore   store.IShiftStore
+	soldierStore store.ISoldierStore
+	userStore    store.IUserStore
+}
+
 func SetupRoutes(v1Router fiber.Router, controllers []Controller) error {
 	for _, controller := range controllers {
 		if err := controller.RegisterRoutes(v1Router); err != nil {
@@ -47,12 +56,64 @@ func SetupRoutes(v1Router fiber.Router, controllers []Controller) error {
 }
 
 func InitControllers() (controllers []Controller, err error) {
-	userStore, err := store.NewUserStore()
+	storeInstances, err := initStoreInstances()
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to initialize user store")
+		return
 	}
 
-	registrationController, err := NewRegistrationController(userStore)
+	authMiddleware := jwt.NewAuthMiddleware(config.JWTSecret)
+
+	registrationController, err := NewRegistrationController(storeInstances.userStore)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to initialize registration controller")
+	}
 	controllers = append(controllers, registrationController)
+
+	dayScheduleController, err := NewDayScheduleController(storeInstances.dayStore, authMiddleware)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to initialize day schedule controller")
+	}
+	controllers = append(controllers, dayScheduleController)
+
+	shiftController, err := NewShiftController(storeInstances.shiftStore, authMiddleware)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to initialize shift controller")
+	}
+	controllers = append(controllers, shiftController)
+
+	soldierController, err := NewSoldierController(storeInstances.soldierStore, authMiddleware)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to initialize shift controller")
+	}
+	controllers = append(controllers, soldierController)
+
 	return
+}
+
+func initStoreInstances() (storeInstancesContainer, error) {
+	userStore, err := store.NewUserStore()
+	if err != nil {
+		return storeInstancesContainer{}, errors.Wrap(err, "failed to initialize user store")
+	}
+
+	daySchedStore, err := store.NewInMemDaySchedStore()
+	if err != nil {
+		return storeInstancesContainer{}, errors.Wrap(err, "failed to initialize day schedule store")
+	}
+
+	shiftStore, err := store.NewShiftStore()
+	if err != nil {
+		return storeInstancesContainer{}, errors.Wrap(err, "failed to initialize shift store")
+	}
+
+	soldierStore, err := store.NewSoldierStore()
+	if err != nil {
+		return storeInstancesContainer{}, errors.Wrap(err, "failed to initialize soldier store")
+	}
+	return storeInstancesContainer{
+		dayStore:     daySchedStore,
+		shiftStore:   shiftStore,
+		soldierStore: soldierStore,
+		userStore:    userStore,
+	}, nil
 }
